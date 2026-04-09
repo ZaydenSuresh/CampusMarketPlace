98440e87e8cddf641833ee69c443173be77a143f
Tagona Danha <2808494@students.wits.ac.za>
2026-04-09 16:39:57 +0200

    User model added

---
+++ models/user.js
@@ -0,0 +1,15 @@
+class User{
+    constructor(id,name,password,email,role){
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email;
+        this.role;
+        
+        
+    }
+}
+
+
+
+module.exports = User;
