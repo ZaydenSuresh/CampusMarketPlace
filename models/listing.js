c38e64a51ab39a2e565621d5d8e5febe6198f848
ZaydenSuresh <2610978@students.wits.ac.za>
2026-04-15 12:39:17 +0200

    Added edit functionality to listing, also refreshing listing

--- models/listing.js
+++ models/listing.js
@@ -2,15 +2,23 @@ const supabase = require('../database'); //Using supabase as database
 
 // Creating the listing using the table listings
 async function createListing(listing) {
+    
+    // Sends a new listing object to the Supabase 'listings' table
+    // and inserts it as a new record in the database, including category, sale_status, etc.
     const { data, error } = await supabase
         .from('listings')
         .insert([listing])
         .select('*');
 
+
     if (error) throw new Error(error.message);
+
+    // Returns the newly created listing (first item in returned array)
     return data[0];
 }
+
 //Other files can use createListing function
 module.exports = {
     createListing
 };
+
